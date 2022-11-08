cb8d2677c6ff8958e877124191de936ab5168e76
minhvu112001 <81789695+minhvu112001@users.noreply.github.com>
2022-11-08 11:45:42 +0700

    helloworld

--- helloworld2.2.js
+++ helloworld2.2.js
@@ -7,17 +7,17 @@ const server = http.createServer((req,res) => {
 '').toLowerCase()
  switch(path) {
  case '':
- res.writeHead(200, { 'Content-Type': 'text/plain' })
- res.end('Homepage')
- break
+   res.writeHead(200, { 'Content-Type': 'text/plain' })
+   res.end('Homepage')
+   break
  case '/about':
- res.writeHead(200, { 'Content-Type': 'text/plain' })
- res.end('About')
- break
+   res.writeHead(200, { 'Content-Type': 'text/plain' })
+   res.end('About')
+   break
  default:
- res.writeHead(404, { 'Content-Type': 'text/plain' })
- res.end('Not Found')
- break
+    res.writeHead(404, { 'Content-Type': 'text/plain' })
+    res.end('Not Found')
+    break
  } })
 server.listen(port, () => console.log(`server started on
 port ${port}; ` +
